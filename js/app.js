0aaac3f56a008ccc78005488b4203f28adba9d4d
Andrea Guffi <andreaguffi92@gmail.com>
2020-10-26 21:54:56 +0100

    casablu page

--- js/app.js
+++ js/app.js
@@ -22,29 +22,6 @@ closeBtn.addEventListener("click", function () {
 date.innerHTML = new Date().getFullYear();
 
 
-// TEXT SCRIPT
-const texts = ['Frontend Developer', 'UX Designer', 'Dreamer', 'Creative'];
-let count = 0;
-let index = 0;
-let currentText = '';
-let letter = '';
-
-(function type() {
-
-  if (count === texts.length) {
-    count = 0;
-  }
-  currentText = texts[count];
-  letter = currentText.slice(0, ++index);
-
-  document.querySelector('.typing').textContent = letter;
-  if (letter.length === currentText.length) {
-    count++;
-    index = 0;
-  }
-  setTimeout(type, 450);
-}());
-
 
 
 
